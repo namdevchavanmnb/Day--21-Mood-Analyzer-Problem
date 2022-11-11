a26a1e770174dbe65759b5454d948b5d7cf89e98
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-11 07:49:14 +0530

    Added:Mood Analyser

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,28 +1,36 @@
 package com.bridgelabz;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 public class MoodAnalyzerTest {
 
     @BeforeAll
-    static void show_msg_before_all_test_case (){
-        System.out.println("---Welcome to Mood Analyzer---");
+    static void show_msg_before_all_test_case() {
+        System.out.println("Welcome to Mood Analyzer");
     }
+
     @AfterAll
-    public static void show_Msg_After_All_Test_Case(){
+    public static void show_Msg_After_All_Test_Case() {
         System.out.println("All Test Case Completed");
     }
+
     @AfterEach
-    void after_Each_Test_Case_Print_Msg(){
+    void after_Each_Test_Case_Print_Msg() {
         System.out.println("Pass");
     }
-    MoodAnalyser object ;
+
+    MoodAnalyser object;
+
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         object = new MoodAnalyser();
+    }
+
+    @Test
+    public void moodAnalyser() {
+        String ans = object.moodAnalyser("I am in Sad Mood");
+        Assertions.assertEquals("SAD", ans);
+        ;
+        System.out.println(ans);
     }
 }
